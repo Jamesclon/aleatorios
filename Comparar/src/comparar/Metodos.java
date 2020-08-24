38c103749b19a200c11d522bfc5bac9f261da65a
Miguel Vega <69647961+Jamesclon@users.noreply.github.com>
2020-08-24 11:45:27 +0200

    First Upload
    Project ended

---
+++ Comparar/src/comparar/Metodos.java
@@ -0,0 +1,20 @@
+package comparar;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Miguel Vega <jamesclon@gmail.com>
+ */
+public class Metodos {
+    
+    public static boolean comparar (int x, int y){
+        return (x>=y);
+    }
+    
+    
+}
